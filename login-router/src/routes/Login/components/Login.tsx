88c99dd8a13d8d07598036ed2bf29bc1f0c108b1
Brandon Gordon <bgordon@linamar.com>
2018-02-08 13:41:25 -0500

    Completed the login form and routing. Now I want to be able to go to the next page (home) after pushing and using history

--- login-router/src/routes/Login/components/Login.tsx
+++ login-router/src/routes/Login/components/Login.tsx
@@ -1,15 +1,50 @@
 ﻿import * as React from 'react';
+import { LoginProps } from '../containers/LoginContainer';
 
-class Login extends React.Component {
+//initialize the initial state of the LoginProps.IState "object" properties
+const initialState: LoginProps.IState = {
+    username: '',
+    password: ''
+};
+
+class Login extends React.Component<LoginProps.IProps, LoginProps.IState> {
+    constructor(props: LoginProps.IProps) {
+        super(props);
+
+        this.state = initialState;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+
+    handleUsernameChange(e: React.FormEvent<HTMLInputElement>) {
+        this.setState({ username: e.currentTarget.value }); //made a mistake here... forgot the curly braces to encompass username
+    }
+
+    handlePasswordChange(e: React.FormEvent<HTMLInputElement>) {
+        this.setState({ password: e.currentTarget.value });
+    }
+
+    handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        //forgot that to refer to the username and password, i use "this.state.variable"
+        if (this.state.username.length > 0 && this.state.password.length > 0) {
+            alert('Submission pressed ' + this.state.username + ' ' + this.state.password);
+        }
+        else {
+            alert('Invalid username or password');
+        }
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label className="label-margin"> Please enter username and password </label>
-                    <br /><br/>
-                    <input type="text" placeholder="Username" className="input-fields" />
+                    <br /><br />
+                    <input type="text" placeholder="Username" className="input-fields" onChange={this.handleUsernameChange} />
                     <br />
-                    <input type="password" placeholder="Password" className="input-fields" />
+                    <input type="password" placeholder="Password" className="input-fields" onChange={this.handlePasswordChange} />
                     <br/>
                     <input type="submit" className="login-button" value="Login" />
                 </form>
--- login-router/src/routes/Login/containers/LoginContainer.tsx
+++ login-router/src/routes/Login/containers/LoginContainer.tsx
@@ -1,1 +1,18 @@
-﻿
+﻿import * as React from 'react';
+
+export namespace LoginProps {
+    export interface IStateProps {
+        errorMessage?: string;
+        error?: boolean;
+    }
+
+    //I believe this is where the login(username, password) function will reside. unsure how it'll work though
+    export interface IDispatchProps { }
+    export interface IOwnProps { } //unsure what this does
+    export interface IProps extends IStateProps, IDispatchProps, IOwnProps { } //does nothing either
+
+    export interface IState {
+        username: string;
+        password: string;
+    }
+}
